c451c8273b8c863bdbe714115442886fa8031fe5
WinterNox <121163909+WinterNox@users.noreply.github.com>
2024-05-20 00:51:45 +0530

    Remove graphical elements, Minor updates

--- Circular Wall/code.lua
+++ Circular Wall/code.lua
@@ -4,15 +4,19 @@ local circular_walls = {}
 local entities = {}
 
 local entity_types = {pewpew.EntityType.ASTEROID, pewpew.EntityType.BAF, pewpew.EntityType.INERTIAC, pewpew.EntityType.MOTHERSHIP, pewpew.EntityType.MOTHERSHIP_BULLET, pewpew.EntityType.ROLLING_CUBE, pewpew.EntityType.ROLLING_SPHERE, pewpew.EntityType.UFO, pewpew.EntityType.WARY, pewpew.EntityType.CROWDER, pewpew.EntityType.CUSTOMIZABLE_ENTITY, pewpew.EntityType.SHIP, pewpew.EntityType.BOMB, pewpew.EntityType.BAF_BLUE, pewpew.EntityType.BAF_RED, pewpew.EntityType.WARY_MISSILE, pewpew.EntityType.UFO_BULLET, pewpew.EntityType.PLAYER_BULLET, pewpew.EntityType.BOMB_EXPLOSION, pewpew.EntityType.PLAYER_EXPLOSION, pewpew.EntityType.BONUS, pewpew.EntityType.FLOATING_MESSAGE, pewpew.EntityType.POINTONIUM, pewpew.EntityType.BONUS_IMPLOSION}
+local entity_types_to_ignore = {pewpew.EntityType.BOMB_EXPLOSION, pewpew.EntityType.BONUS, pewpew.EntityType.BONUS_IMPLOSION, pewpew.EntityType.PLAYER_EXPLOSION, pewpew.EntityType.FLOATING_MESSAGE, pewpew.EntityType.POINTONIUM}
 
-local approximate_radii = require("/dynamic/Circular Wall/approx_radii.lua")
-local approximate_speeds = require("/dynamic/Circular Wall/approx_speeds.lua")
+local approximate_radii
+local approximate_speeds
+
+local wall_count
+local speed_mult = 1fx
 
 local function circular_wall_update_callback(entity_id)
   local wall = circular_walls[entity_id]
 
   for entity, wall_id in pairs(wall.temp_walls) do
-    if not pewpew.entity_get_is_alive(entity) then
+    if (not pewpew.entity_get_is_alive(entity)) or pewpew.entity_get_is_started_to_be_destroyed(entity) then
       pewpew.remove_wall(wall_id)
       wall.temp_walls[entity] = nil
     end
@@ -34,9 +38,10 @@ local function circular_wall_update_callback(entity_id)
       local sqr_distance = dx * dx + dy * dy
       local distance = fmath.sqrt(sqr_distance)
 
-      local entity_radius = approximate_radii[pewpew.get_entity_type(entity)] + approximate_speeds[pewpew.get_entity_type(entity)]
+      local entity_radius = approximate_radii[pewpew.get_entity_type(entity)] + approximate_speeds[pewpew.get_entity_type(entity)] * speed_mult
+      local wall_size = approximate_radii[pewpew.get_entity_type(entity)] + approximate_speeds[pewpew.get_entity_type(entity)]
 
-      if distance < wall.radius + entity_radius then  -- Using 3fx/2fx as a safety measure
+      if distance < wall.radius + entity_radius then  -- Using (2fx - fmath.to_fixedpoint(wall_count) / 200fx) to have higher accuracy when there are less walls and less usage of resources when there are more
         local tangent_angle = fmath.atan2(dy, dx) + fmath.tau()/4fx
 
         local increment_y, increment_x = fmath.sincos(tangent_angle)
@@ -49,10 +54,10 @@ local function circular_wall_update_callback(entity_id)
         end
 
         local default_wall_id = pewpew.add_wall(
-          wall.pos.x + dx + increment_x * entity_radius / 2fx,
-          wall.pos.y + dy + increment_y * entity_radius / 2fx,
-          wall.pos.x + dx - increment_x * entity_radius / 2fx,
-          wall.pos.y + dy - increment_y * entity_radius / 2fx
+          wall.pos.x + dx + increment_x * wall_size / 2fx,
+          wall.pos.y + dy + increment_y * wall_size / 2fx,
+          wall.pos.x + dx - increment_x * wall_size / 2fx,
+          wall.pos.y + dy - increment_y * wall_size / 2fx
         )
 
         if default_wall_id ~= 65535 then
@@ -68,19 +73,27 @@ local function circular_wall_update_callback(entity_id)
   end
 end
 
-local is_ufos_collision_enabled = false
-local is_rolling_cubes_collision_enabled = false
+-- Initializes the module.
+-- @params configuration: table; the table containing the different optional and non optional parameters for the module
 function circular_wall.init(configuration)
+  circular_wall.path = configuration.path
+  if configuration.path == nil then
+    circular_wall.path = "/dynamic/"
+  end
+
+  approximate_radii = require(circular_wall.path.."Circular Wall/approx_radii.lua")
+  approximate_speeds = require(circular_wall.path.."Circular Wall/approx_speeds.lua")
+
   for entity_type, value in pairs(configuration.max_speeds) do
     approximate_speeds[entity_type] = value
   end
 
   if configuration.is_ufos_collision_enabled == true then
-    is_ufos_collision_enabled = true
+    table.insert(entity_types_to_ignore, pewpew.EntityType.UFO)
   end
 
   if configuration.is_rolling_cubes_collision_enabled == true then
-    is_rolling_cubes_collision_enabled = true
+    table.insert(entity_types_to_ignore, pewpew.EntityType.ROLLING_CUBE)
   end
 
   level_width = configuration.level_width
@@ -91,15 +104,17 @@ end
 -- @params x: FixedPoint the x position of the center of the circular wall
 -- @params y: FixedPoint the y position of the center of the circular wall
 -- @params radius: FixedPoint the radius of the circular wall
--- @params color: a 32 bit color; the color of the circular wall
-function circular_wall.new(x, y, radius, color)
+--[[
+  @params mesh: table {
+  file_path: String the path to the file containing the meshes
+  index: int the index of the mesh (starting from 0)
+  radius: FixedPoint the optional visibility radius to be applied
+]]--
+function circular_wall.new(x, y, radius, mesh)
   local entity_id = pewpew.new_customizable_entity(x, y)
 
   -- Set the entity's properties
-  pewpew.customizable_entity_set_mesh(entity_id, "/dynamic/Circular Wall/graphics.lua", 0)
-  pewpew.customizable_entity_set_mesh_scale(entity_id, radius)  -- The mesh is a unit circle
-  pewpew.customizable_entity_set_mesh_color(entity_id, color)  -- The mesh is white, any multiplication to it will make the entity that color.
-  pewpew.customizable_entity_set_visibility_radius(entity_id, radius)
+  circular_wall.set_mesh(entity_id, mesh)
 
   circular_walls[entity_id] = {
     pos = {
@@ -115,6 +130,41 @@ function circular_wall.new(x, y, radius, color)
   return entity_id
 end
 
+-- Updates the position of a circular wall.
+-- @params entity_id: EntityID; the id of the circular wall, obtained by circular_Wall.new()
+-- @params x: FixedPoint; the x component of the position to update to
+-- @params y: FixedPoint; the y component of the position to update to
+function circular_wall.set_pos(entity_id, x, y)
+  circular_walls[entity_id].pos = {x, y}
+end
+
+-- Updates the radius of a circular wall.
+-- @params entity_id: EntityID; the id of the circular wall, obtained by circular_Wall.new()
+-- @params radius: FixedPoint; the radius to update to
+function circular_wall.set_radius(entity_id, radius)
+  circular_walls[entity_id].radius = radius
+end
+
+-- Sets the mesh of a circular wall
+-- @params entity_id: EntityID; the id of the circular wall, obtained by circular_Wall.new()
+--[[
+  @params mesh: table {
+  file_path: String the path to the file containing the meshes
+  index: int the index of the mesh (starting from 0)
+  radius: FixedPoint the optional visibility radius to be applied
+]]--
+function circular_wall.set_mesh(entity_id, mesh)
+  if mesh ~= nil then
+    if mesh.file_path and mesh.index then
+      pewpew.customizable_entity_set_mesh(entity_id, mesh.file_path, mesh.index)
+
+      if mesh.visibility_radius ~= nil then
+        pewpew.customizable_entity_set_visibility_radius(entity_id, mesh.visibility_radius)
+      end
+    end
+  end
+end
+
 -- Returns the number of walls being used by the module.
 function circular_wall.get_wall_count()
   local wall_num = 0
@@ -123,6 +173,8 @@ function circular_wall.get_wall_count()
       wall_num = wall_num + 1
     end
   end
+
+  wall_count = wall_num
 
   return wall_num
 end
@@ -150,7 +202,7 @@ end
 function circular_wall.random_position(entity_type)
   local x
   local y
-  local entity_radius = approximate_radii[entity_type] + approximate_speeds[entity_type]
+  local entity_radius = approximate_radii[entity_type]
 
   local num_outside_walls = 0
   local circular_walls_num = 0
@@ -237,11 +289,30 @@ local function update_entity_list()
   local entities_temp = pewpew.get_all_entities()
 
   for i = #entities_temp, 1, -1 do
+    -- Check if entity is exploding
+    if pewpew.entity_get_is_started_to_be_destroyed(entities_temp[i]) then
+      table.remove(entities_temp, i) 
+      goto continue
+    end
+    
+    -- Check if entity type is supposed to be ignored, remove if so
+    local entity_type = pewpew.get_entity_type(entities_temp[i])
+    for j = 1, #entity_types_to_ignore do
+      if entity_type == entity_types_to_ignore[j] then
+        table.remove(entities_temp, i)
+        goto continue
+      end
+    end
+
+    -- Check if entity is a circular wall, remove if so
     for cw_entity_id, wall_information in pairs(circular_walls) do
       if entities_temp[i] == cw_entity_id then
         table.remove(entities_temp, i)
+        goto continue
       end
     end
+
+    ::continue::
   end
 
   entities = entities_temp
--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Circular Walls (PewPew Live)
 
-Circular walls is a module that allows level creators to create almost perfect circular wall collisions in PewPew Live without using a lot of resources or needing more than limit of 200 walls. It can be used extremely easily even by the people who have just stepped into level creation in PewPew Live.
+**Circular Walls** is a module that allows level creators to create almost perfect circular wall collisions in PewPew Live without using a lot of resources or needing more than the limit of 200 walls. It can be used extremely easily even by the people who have just stepped into level creation in PewPew Live. Though, keep in mind that this module only provides the functionality of circular walls and no visual elements. The mesh for the walls are to be created manually by the level creator.
+
+**Note: The module only provides the functionality of the walls, not any graphical elements are included.**
 
 ## Installation
 
 To install this module, follow the steps below:
 
 1. Download the source code as a ZIP file.
-2. Move the folder `Circular Walls > Circular Wall` to your level folder.
-3. In `level.lua` or any other file that may use the module's features, add the following line of code:
+2. Extract the file into a desired location.
+3. Move the folder `Circular-Walls-PewPew-Live/Circular Wall` to your level folder.
+4. In `level.lua` or any other file that may use the module's features, add the following line of code. Note that you can move the above folder to any desired location. In that case, you will use the complete path to `Circular Wall/code.lua` inside that folder and during the configuration of the module, set `path` to the parent folder of `Circular Wall`.
 ```lua
 local circular_wall = require("/dynamic/Circular Wall/code.lua")
 ```
@@ -21,17 +24,21 @@ The module is extremely simple to use. To use the module in your level without a
 
 1. Configure the module.
 The module needs to be initialized through a certain function to fit the level's needs properly. If not initialized, the module will break. It is used to gather the following information in order to properly perform with minimal resources:
-    * The **maximum speeds** at which entities of specific types move
+    * `path`: The path to the parent folder of `Circular Wall`; Example: `/dynamic/modules/`. It should end with a forward slash. You can ignore setting this if `Circular Wall` is present in the same folder as `level.lua`.
+    * `max_speeds`: The **maximum speeds** at which entities of specific types move
         * This should only be used to set the maximum speeds of the player ship, customizable entities, and entities whose speed can be controlled. This includes BAFs, Inertiacs, Rolling Spheres, etc. 
         * Setting the maximum speed for entity types that have a fixed speed throughout the game is discouraged. 
         * Setting the maximum speed for entity types that do not collide with walls will have no effect. 
         * You should set the maximum speed for customizable entities to the **maximum** speed of the **fastest** customizable entity that will be used in the level.
-    * The radius of the **largest** customizable entity that will be used in the level
-    * Whether or not UFOs and Rolling Cubes have collision enabled
-    * The level's width and height
+    * `maximum_radius_customizable_entity`: The radius of the **largest** customizable entity that will be used in the level
+    * `is_ufos_collision_enabled, is_rolling_cubes_collision_enabled`: Whether or not UFOs and Rolling Cubes have collision enabled
+    * `level_width, level_height`: The level's width and height
+
+Example use
 ```lua
 circular_wall.init(
   configuration: table {
+    path: String
     max_speeds: table {
       [pewpew.EntityType.BAF]: FixedPoint,
       [pewpew.EntityType.ROLLING_SPHERE]: FixedPoint,
@@ -57,17 +64,21 @@ circular_Wall.new(
   x: FixedPoint,
   y: FixedPoint,
   radius: FixedPoint,
-  color: int
+  mesh: table {
+    file_path: String,
+    index: int,
+    visibility_radius: FixedPoint
+  }
 ): EntityID
 ```
 
-Creates a circular wall entity with its center at location `x`, `y`, radius equal to `radius` and color set to `color` and returns its entityID.
+Creates a circular wall entity with its center at location `x`, `y`, radius equal to `radius` and returns its entityID. If `radius` is negative, the wall will be pass-through. Avoid using negative radii. `mesh` is an optional parameter that specifies a mesh to use with an optinal `visibility_radius` which will be used to set the entity's visibility radius for optimized rendering. It is better to not set the visibility radius if your wall's mesh is extended significantly in the negative Z axis.
 
 ### `circular_wall.get_wall_count()`
 ```lua
 circular_wall.get_wall_count(): int
 ```
-Returns the number of walls that are being used by the module to perform circular wall collisions. The limit to how many walls can be present at a given time is 200. This information can be used to make changes to your level or to the parameters in `circular_wall.init()` if required.
+Returns the number of walls that are being used by the module to perform circular wall collisions. The limit to how many walls can be present at a given time is 200. This information can be used to make changes to your level to ensure that it does not pass the limit.
 
 ### `circular_wall.is_inside_wall()`
 ```lua
@@ -100,9 +111,15 @@ circular_wall.random_position_custom_radius(
 ```
 Returns a location for spawning an entity of type `pewpew.EntityType.CUSTOMIZABLE_ENTITY` such that it will not be inside any of the circular walls present in the level at that time. Returns the origin location if it fails to find a proper location in under 10,000 checks.
 
-**Example**
+**Example:**
 ```lua
 local x, y = circular_wall.random_position_custom_radius(128fx)
 large_entity.new(x, y)
 ```
-Here, the function `large.new(x: FixedPoint, y: FixedPoint)` creates an entity at location `x`, `y`. The collision or visual radius of the entity is `128fx`.
+Here, the function `large.new(x: FixedPoint, y: FixedPoint)` creates an entity at location `x`, `y`. The collision radius of the entity is `128fx`.
+
+## Recommendations
+You should take into consideration the following recommendations:
+1. Add code for manual garbage collection every tick. Since the module makes the use of temporary walls to simulate collisions, it will take up a significant amount of memory. Usually, PewPew Live collects garbage incrementally. To ensure that the temporary walls are deleted from the memory as soon as they are not being used, you can call `collectgarbage("collect")` every tick in your update callback. This will ensure that the module does not limit your usage of memory unnecessarilly.
+
+2. If only few circular walls are required, it is better to just lay down an approximate polygonal wall instead of using this module.
